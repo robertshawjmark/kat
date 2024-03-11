656c03c0dcd737c76125edfcfbc2d12cf553c38d
Mark Robertshaw <robertshawjmark@gmail.com>
2024-03-11 18:48:06 +0000

    update

--- src/__main__.py
+++ src/__main__.py
@@ -4,50 +4,25 @@ import pandas as pd
 
 ROOT = Path().cwd()
 
-
-# headers = [
-#     'Date',
-#     'Withdrawals',
-#     'Deposits',
-#     'Payee',
-#     'Description',
-#     'Reference Number',
-# ]
-
 data = pd.read_csv(
     ROOT.joinpath("data", "VISA GBP Transactions - Downloaded from Bank.csv")
 )
 data.columns = [c.strip() for c in data.columns]
 
-drop = [
-    "Account Number",
-    "Sort Code",
-    "Card",
-    "Ledger Balance",
-]
-
-data = data.drop(columns=drop)
-
 data["Withdrawals"] = data["Amount"].apply(lambda x: -x if x < 0 else 0)
 data["Deposits"] = data["Amount"].apply(lambda x: x if x > 0 else 0)
 data["Payee"] = data["Amount"].apply(lambda x: x if x > 0 else 0)
 
-
 headers = [
     "Date",
-    "Account Number",  # b
-    "Sort Code",  # c
-    "Card",  # d
-    "Narrative",
-    "Amount",
-    "Ledger Balance",  # g
+    "Withdrawals",
+    "Deposits",
+    "Payee",
+    "Description",
+    "Reference Number",
 ]
 
-filter = [
-    "Account Number",
-    "Sort Code",
-    "Card",
-    "Ledger Balance",
-]
 
+data = data[headers]
+data.to_csv("./poo.csv")
 print(data)
